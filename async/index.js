26c68b0e2b3d21cab414353f227b4921619f96e8
mtso <mtso@tuta.io>
2017-02-23 23:58:24 -0800

    Hard-code async to understand test behavior

--- async/index.js
+++ async/index.js
@@ -1,11 +1,28 @@
 var async = {};
 
-async.sequence = function(funcs) {
-  return function(callback) {
-    funcs.forEach(function(func) {
-      func(callback);
-    });
-  }
-}
+// call each function in sequence
+// passing the result of the previous
+// into the next
+async.sequence = function(funcSequence) {
+  
+  // WIP: hard-coded attempt to understand assumptions and test behavior......
+
+  // Returns a function that takes in a callback with the signature: `function(err, data) {}`
+  return function(doneFunc) {
+    // Signature of each function in sequence is `function(callback, data)`
+    funcSequence[0](function(err, data) {
+      // Second function is passed into the callback parameter of the first, along with the data parameter
+      funcSequence[1](function(err, data) {
+        // The last function should invoke the `doneFunc` callback
+        doneFunc.call(null, err, data);
+
+      }, data); // `data` parameter from the first func's callback
+
+    }, undefined); // What is the first function invoked with for data?
+  };
+
+  // NOTE: for each function
+  // invoke, passing in a callback?
+};
 
 module.exports = async;
