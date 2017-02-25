a02a758b7566b15360fd800c199c9647fa1e6166
mtso <mtso@tuta.io>
2017-02-24 21:30:26 -0800

    Refactor async.sequence

--- async/index.js
+++ async/index.js
@@ -1,50 +1,29 @@
 var async = {};
 
-// call each function in sequence
-// passing the result of the previous
-// into the next
+// we need to return a function that, when called, calls each function in the sequence
+// the callback in each function should be passed a function that runs the next function
+// with the data that was returned in the previous callback
+// for the last function callback, we need to invoke the done callback and pass in the data
 async.sequence = function(funcSequence) {
-  // we need to return a function that, when called, calls each function in the sequence
-  // the callback in each function should be passed a function that runs the next function
-  // with the data that was returned in the previous callback
-  // for the last function callback, we need to invoke the done callback and pass in the data
-
-  var iter = 0;
-  var done;
-  var context;
-
-  function makeCallback() {
-    return function(err, data) {
-      if (iter === funcSequence.length) {
-        return done.call(context, err, data);
-      } else {
-        var current = funcSequence[iter];
-        iter++;
-        return current(makeCallback(), data);
-      }
-    };
-  }
-  // function wrap(func) {
-  //   var iter = 1;
-  //   return function(err, data) {
-  //     if (func === undefined) {
-  //       return done.call(context, err, data);
-  //     } else {
-  //       var current = funcSequence[iter];
-  //       iter += 1;
-  //       var next = funcSequence[iter];
-  //       return current(next, data);
-  //     }
-  //   }
-  // }
 
   // Returns a function that takes in a callback with the signature: `function(err, data) {}`
   return function(doneFunc) {
-    context = this;
-    done = doneFunc;
+    var depth = 0;
+    var context = this;
 
-    // return wrap(funcSequence[0]);
-    return funcSequence[iter](makeCallback());
+    function makeCallback() {
+      return function(err, data) {
+        if (depth === funcSequence.length) {
+          return doneFunc.call(context, err, data);
+        } else {
+          var current = funcSequence[depth];
+          depth += 1;
+          return current(makeCallback(), data);
+        }
+      };
+    }
+
+    return funcSequence[depth](makeCallback());
   };
 };
 
